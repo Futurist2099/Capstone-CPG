23776bfe50d178b87a824c139da96b04b5023a0d
17thevision <17thevision@gmail.com>
2026-07-13 18:08:57 -0700

    Rename employee checkin and verify

---
+++ 3_employee_checkin.py
@@ -0,0 +1,11 @@
+# Static variables
+company = "CPG"
+employee_file = "employee.txt"
+
+# Dynamic variables
+name = input("What is your name? ")
+check_in = input("When is your last check in at work? ")
+
+print("Company:", company)
+print("Employee:", name)
+print("Last check in:", check_in)
